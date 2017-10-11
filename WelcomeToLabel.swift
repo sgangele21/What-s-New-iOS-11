f2a7769ef30f9baed6db1bc5fbac67b9234c5355
Sgangele21 <sahil.gangele@gmail.com>
2017-10-11 18:36:05 -0400

    Created What's New Viewcontroller that is going to house all the elements.

--- WelcomeToLabel.swift
+++ WelcomeToLabel.swift
@@ -67,7 +67,7 @@ public class WelcomeToLabel: UILabel {
     }
     
     public required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
 
 }
--- WhatsNewViewController.swift
+++ WhatsNewViewController.swift
@@ -1,32 +1,35 @@
 //
-//  ViewController.swift
+//  WhatsNewViewController.swift
 //  What's New Page
 //
-//  Created by Sahil Gangele on 10/4/17.
+//  Created by Sahil Gangele on 10/11/17.
 //  Copyright © 2017 Sahil Gangele. All rights reserved.
 //
 
 import UIKit
 
-
-class ViewController: UIViewController {
+public class WhatsNewViewController: UIViewController {
     
     let purpleColor = UIColor(displayP3Red: 113.0/255.0, green: 35.0/255.0, blue: 216.0/255.0, alpha: 1.0)
     
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
         
+        let screenWidth = self.view.frame.width
+        let customWidth = screenWidth - (screenWidth / 8.0)
+        
         // Label
-        let whatsNewLabel = WelcomeToLabel(frame: CGRect(x: 0, y: 0, width: 100, height: 100), title: "Podcasts", tintColor: self.purpleColor)
+        let whatsNewLabel = WelcomeToLabel(frame: CGRect(x: 0, y: 0, width: customWidth + 10.0, height: 100), title: "Podcasts", tintColor: self.purpleColor)
+        whatsNewLabel.font = UIFont.systemFont(ofSize: 40.0, weight: .heavy)
         whatsNewLabel.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(whatsNewLabel)
         whatsNewLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 90).isActive = true
-        whatsNewLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: -25).isActive = true
+        whatsNewLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
         
         // Button
         let button = ClosePageButton(frame: CGRect(x: 0, y: 0, width: 300, height: 55), backgroundColor: self.purpleColor, buttonTitle: "Start Listening")
         self.view.addSubview(button)
-        button.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -60).isActive = true
+        button.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -35).isActive = true
         button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
         
         // Description One
@@ -40,15 +43,18 @@ class ViewController: UIViewController {
         let descriptionViewThree = DescriptionView.loadFromNib(frame: frame, descriptionTitle: "Download for later", description: "Play podcasts offline by saving them for later.", image: #imageLiteral(resourceName: "Download"))
         
         // Description Stack View
-        let stackView = DescriptionStackView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width - 10.0, height: (frame.height * 3) + 30.0))
+        let stackView = DescriptionStackView(frame: CGRect(x: 0, y: 0, width: customWidth, height: (frame.height * 3) + 30.0))
         stackView.addArrangedDescriptionView(descriptionView: descriptionViewOne)
         stackView.addArrangedDescriptionView(descriptionView: descriptionViewTwo)
         stackView.addArrangedDescriptionView(descriptionView: descriptionViewThree)
         self.view.addSubview(stackView)
-        stackView.allignInMiddle(view: self.view, constant: 10.0)
+        stackView.allignInMiddle(view: self.view)
+        
+        whatsNewLabel.widthAnchor.constraint(equalTo: stackView.widthAnchor).isActive = true
         
     }
-
+    
+    
 }
 
 extension UIView {
@@ -61,3 +67,4 @@ extension UIView {
     }
     
 }
+
